c6f68ef78ec2047d1c105bab258f5ed4ab050540
Abhay Sharma <abhaysharma20dec@gmail.com>
2025-07-26 09:51:21 +0530

    UI completed

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* ======================= typing animation ======================= */
+var typed = new Typed('.typing', {
+    strings: ["","Java Web Developer", "Android Developer", "Software Engineer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
